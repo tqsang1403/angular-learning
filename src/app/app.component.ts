eaa6ae843f86d0fa25f5c020bcf084d4c58fd588
tqsang1403 <sangtranhd20@gmail.com>
2022-08-19 14:51:12 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+
+  user = {
+    name: 'Sang',
+    age: 18,
+  };
+
+  handler() {
+    console.log('clicked');
+  }
 }
